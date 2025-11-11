7cfbf0b0278d57438df5dbc4f49b962e36d658a6
ankushmahajan562-art <ankushmahajan562@gmail.com>
2025-11-11 16:48:56 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const joinButton = document.querySelector("button");
+
+  joinButton.addEventListener("click", function() {
+    alert("🔥 Welcome to Sehaj Fitness! Let's begin your journey! 💪");
+  });
+});
